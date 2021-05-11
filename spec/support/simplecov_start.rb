beea980aa1d55e578a3ef556df4b25020f52623d
Dmitry Galinsky <dima.exe@gmail.com>
2021-05-11 20:43:31 +0300

    update actions

--- spec/support/simplecov_start.rb
+++ spec/support/simplecov_start.rb
@@ -1,22 +1,14 @@
 # frozen_string_literal: true
 
 require 'simplecov'
+require 'simplecov_json_formatter'
+
+SimpleCov.formatters = [
+  SimpleCov::Formatter::JSONFormatter,
+  SimpleCov::Formatter::HTMLFormatter
+]
 
 SimpleCov.start do
   add_filter '/spec/'
-
   track_files 'lib/**/*.rb'
-
-  if ENV['TEAMCITY_VERSION']
-    at_exit do
-      SimpleCov.result.format!
-
-      puts <<~SUMMARY
-        ##teamcity[blockOpened name='Code Coverage Summary']
-        ##teamcity[buildStatisticValue key='CodeCoverageAbsLCovered' value='#{SimpleCov.result.covered_lines}']
-        ##teamcity[buildStatisticValue key='CodeCoverageAbsLTotal' value='#{SimpleCov.result.total_lines}']
-        ##teamcity[blockClosed name='Code Coverage Summary']
-      SUMMARY
-    end
-  end
 end
