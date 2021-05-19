bbabfe75f91bd902d261a00e5701ea7926508668
Dmitry Galinsky <dima.exe@gmail.com>
2021-05-19 20:47:13 +0300

    update RubyVM collector

---
+++ lib/bm/instrumentations/ruby_vm/collector.rb
@@ -0,0 +1,86 @@
+# frozen_string_literal: true
+
+require_relative 'metrics_collection'
+
+module BM
+  module Instrumentations
+    # Collects Ruby VM and GC metrics
+    module RubyVM
+      # Collects Ruby VM and GC metrics
+      #
+      # @attr [MetricsCollection] metrics_collection
+      class Collector
+        attr_reader :metrics_collection
+
+        SLOTS_FREE = { labels: { slots: 'free' } }.freeze
+        SLOTS_LIVE = { labels: { slots: 'live' } }.freeze
+        CACHE_METHOD = { labels: { cache: 'method' } }.freeze
+        CACHE_CONSTANT = { labels: { cache: 'constant' } }.freeze
+
+        # @param registry [Prometheus::Client::Registry]
+        # @api private
+        def initialize(registry)
+          @metrics_collection = MetricsCollection.new(registry)
+        end
+
+        # @return [void]
+        def update
+          update_gc_profiler_total_time if GC::Profiler.enabled?
+          update_gc_stats(::GC.stat)
+          update_global_cache(::RubyVM.stat)
+          metrics_collection.threads_count.set(::Thread.list.size)
+        end
+
+        # @return [Proc]
+        def to_proc
+          -> { update }
+        end
+
+        private
+
+        # @param gc_stats [Hash<Symbol, Integer>]
+        # @option gc_stats [Integer] :heap_free_slots
+        # @option gc_stats [Integer] :heap_live_slots
+        # @option gc_stats [Integer] :total_allocated_objects
+        # @option gc_stats [Integer] :total_freed_objects
+        def update_gc_stats(gc_stats)
+          metrics_collection.gc_heap_slots_size.set(gc_stats[:heap_free_slots], SLOTS_FREE)
+          metrics_collection.gc_heap_slots_size.set(gc_stats[:heap_live_slots], SLOTS_LIVE)
+          metrics_collection.gc_allocated_objects_total.set(gc_stats[:total_allocated_objects])
+          metrics_collection.gc_freed_objects_total.set(gc_stats[:total_freed_objects])
+        end
+
+        # @param vm_stats [Hash<Symbol, Integer>]
+        # @option vm_stats [Integer] :global_method_state
+        # @option vm_stats [Integer] :global_constant_state
+        def update_global_cache(vm_stats)
+          return unless %i[global_method_state global_constant_state].all? { vm_stats.include?(_1) }
+
+          metrics_collection.vm_global_cache_state.set(vm_stats[:global_method_state], CACHE_METHOD)
+          metrics_collection.vm_global_cache_state.set(vm_stats[:global_constant_state], CACHE_CONSTANT)
+        end
+
+        # Records {GC::Profiler.total_time} if it's enabled
+        #
+        # By default {GC::Profile} is disabled and total time always zero, to get non zero values
+        # the GC profiler should be enabled by calling {GC::Profiler.enable}
+        def update_gc_profiler_total_time
+          total_time = GC::Profiler.total_time
+          metrics_collection.gc_time_seconds.observe(total_time.round(6))
+        ensure
+          GC::Profiler.clear
+        end
+      end
+
+      # @param registry [Prometheus::Client::Registry]
+      # @param enable_gc_profiler [Boolean]
+      # @return [void]
+      def self.install(registry = nil, enable_gc_profiler: false)
+        ::GC::Profiler.enable if enable_gc_profiler
+
+        registry ||= Prometheus::Client.registry
+        registry.add_custom_collector(&Collector.new(registry))
+      end
+    end
+  end
+end
---
+++ lib/bm/instrumentations/ruby_vm/metrics_collection.rb
@@ -0,0 +1,119 @@
+# frozen_string_literal: true
+
+module BM
+  module Instrumentations
+    module RubyVM
+      # @attr [Prometheus::Client::Summary] gc_time_seconds
+      # @attr [Prometheus::Client::Gauge] gc_heap_slots_size
+      # @attr [Prometheus::Client::Gauge] gc_allocated_objects_total
+      # @attr [Prometheus::Client::Gauge] gc_freed_objects_total
+      # @attr [Prometheus::Client::Gauge] gc_counts_total
+      # @attr [Prometheus::Client::Gauge] vm_global_cache_state
+      # @attr [Prometheus::Client::Gauge] threads_count
+      class MetricsCollection
+        include RegisterMetric
+
+        attr_reader :gc_time_seconds, :gc_heap_slots_size, :gc_allocated_objects_total,
+                    :gc_freed_objects_total, :gc_counts_total, :vm_global_cache_state, :threads_count
+
+        def initialize(registry)
+          build_version(registry)
+          build_gc_time_seconds(registry)
+          build_gc_heap_slots_size(registry)
+          build_gc_allocated_objects_total(registry)
+          build_gc_freed_objects_total(registry)
+          build_gc_counts_total(registry)
+          build_vm_global_cache_state(registry)
+          build_threads_count(registry)
+        end
+
+        private
+
+        # @param registry [Prometheus::Client::Registry]
+        def build_version(registry)
+          version = register_metric(registry, :ruby_version) do |name|
+            registry.gauge(
+              name,
+              docstring: 'The Ruby VM version',
+              labels: %i[ruby version]
+            )
+          end
+          version.set(1, labels: { ruby: RUBY_ENGINE, version: RUBY_ENGINE_VERSION })
+        end
+
+        # @param registry [Prometheus::Client::Registry]
+        def build_gc_time_seconds(registry)
+          @gc_time_seconds = register_metric(registry, :ruby_gc_time_seconds) do |name|
+            registry.summary(
+              name,
+              docstring: 'The total time that Ruby GC spends for garbage collection in seconds'
+            )
+          end
+        end
+
+        # @param registry [Prometheus::Client::Registry]
+        def build_gc_heap_slots_size(registry)
+          @gc_heap_slots_size = register_metric(registry, :ruby_gc_heap_slots_size) do |name|
+            registry.gauge(
+              name,
+              docstring: 'The size of available heap slots of Ruby GC partitioned by type',
+              labels: %i[slot_type]
+            )
+          end
+        end
+
+        # @param registry [Prometheus::Client::Registry]
+        def build_gc_allocated_objects_total(registry)
+          @gc_allocated_objects_total = register_metric(registry, :ruby_gc_allocated_objects_total) do |name|
+            registry.gauge(
+              name,
+              docstring: 'The total number of allocated objects by Ruby GC'
+            )
+          end
+        end
+
+        # @param registry [Prometheus::Client::Registry]
+        def build_gc_freed_objects_total(registry)
+          @gc_freed_objects_total = register_metric(registry, :ruby_gc_freed_objects_total) do |name|
+            registry.gauge(
+              name,
+              docstring: 'The total number of freed objects by Ruby GC'
+            )
+          end
+        end
+
+        # @param registry [Prometheus::Client::Registry]
+        def build_gc_counts_total(registry)
+          @gc_counts_total = register_metric(registry, :ruby_gc_counts_total) do |name|
+            registry.gauge(
+              name,
+              docstring: 'The total number of Ruby GC counts partitioned by type',
+              labels: %i[count_type]
+            )
+          end
+        end
+
+        # @param registry [Prometheus::Client::Registry]
+        def build_vm_global_cache_state(registry)
+          @vm_global_cache_state = register_metric(registry, :ruby_vm_global_cache_state) do |name|
+            registry.gauge(
+              name,
+              docstring: 'The Ruby VM global cache state (version) for methods and constants, partitioned by type',
+              labels: %i[cache_type]
+            )
+          end
+        end
+
+        # @param registry [Prometheus::Client::Registry]
+        def build_threads_count(registry)
+          @threads_count = register_metric(registry, :ruby_threads_count) do |name|
+            registry.gauge(
+              name,
+              docstring: 'The number of running threads'
+            )
+          end
+        end
+      end
+    end
+  end
+end
